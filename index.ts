c932ccceb9680103de36078bafc431503cbf544a
Mạnh <creshtthuyquai@gmail.com>
2025-09-15 21:31:50 +0700

    connect db

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("ok");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+dotenv.config();
+database.connect();
+const app: Express = express();
+const port: number | string = process.env.PORT;
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.get("/topics", (req: Request, res: Response) => {
+  res.render("client/pages/topics/index.pug");
+});
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
