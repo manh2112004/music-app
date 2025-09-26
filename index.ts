1be8d62a1ae59aa55e8cb0f723f02f4e6b58edc0
Mạnh <creshtthuyquai@gmail.com>
2025-09-26 20:48:37 +0700

    fix deploy for vercel

--- index.ts
+++ index.ts
@@ -6,26 +6,36 @@ import adminRoutes from "./routes/admin/index-route";
 import { systemConfig } from "./config/config";
 import path from "path";
 import methodOverride from "method-override";
+
 dotenv.config();
 database.connect();
+
 const app: Express = express();
-const port: number | string = process.env.PORT || 3000;
+
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(`${__dirname}/public`));
-app.set("views", `${__dirname}/views`);
+
+// static + view
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
 // tinymce
 app.use(
   "/tinymce",
   express.static(path.join(__dirname, "node_modules", "tinymce"))
 );
+
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 // client routes
 clientRoutes(app);
+
 // admin routes
 adminRoutes(app);
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-});
+
+// ❌ Không dùng app.listen()
+// ✅ Export app cho Vercel
+export default app;
